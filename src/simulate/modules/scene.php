963029d56ad73f33b809806cb226b7adc46428c4
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2024-01-31 23:16:21 +0100

    added HP and damage information

---
+++ src/simulate/modules/scene.php
@@ -0,0 +1,62 @@
+<div class="scene">
+	<div class="background"></div>
+	<div class="pokemon-container self">
+		<div class="messages"></div>
+		<h3 class="name">Venusaur</h3>
+		<div class="hp bar-back">
+		    <div class="hp-loss">-2</div>
+		    <div class="hp-value">
+		        <span class="hp-now">60</span>
+		        /
+		        <span class="hp-stat">120</span>
+		    </div>
+			<div class="bar damage"></div>
+			<div class="bar"></div>
+		</div>
+		<div class="pokemon">
+			<div class="fast-move-circle-container">
+				<div class="fast-move-circle"></div>
+			</div>
+			<div class="shadow"></div>
+			<div class="main-sprite"></div>
+			<div class="secondary-sprite"></div>
+			<div class="white-sprite"></div>
+			<div class="shield-sprite-container">
+				<div class="shield-sprite"></div>
+				<div class="shield-sprite"></div>
+				<div class="shield-sprite"></div>
+				<div class="shield-sprite"></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="pokemon-container opponent">
+		<div class="messages"></div>
+		<h3 class="name">Skarmory</h3>
+		<div class="hp bar-back">
+		    <div class="hp-loss">-4</div>
+		    <div class="hp-value">
+		        <span class="hp-now">80</span>
+		        /
+		        <span class="hp-stat">100</span>
+		    </div>
+			<div class="bar damage"></div>
+			<div class="bar"></div>
+		</div>
+		<div class="pokemon">
+			<div class="fast-move-circle-container">
+				<div class="fast-move-circle"></div>
+			</div>
+			<div class="shadow"></div>
+			<div class="main-sprite"></div>
+			<div class="secondary-sprite"></div>
+			<div class="white-sprite"></div>
+			<div class="shield-sprite-container">
+				<div class="shield-sprite"></div>
+				<div class="shield-sprite"></div>
+				<div class="shield-sprite"></div>
+				<div class="shield-sprite"></div>
+			</div>
+		</div>
+	</div>
+</div>
