957f1545a23b84f9d9e6beae213982fbb151f35e
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2024-01-31 23:17:29 +0100

    added CP, IV and rank info

---
+++ src/simulate/modules/top.php
@@ -0,0 +1,47 @@
+<div class="top">
+	<div class="timer">
+		<div class="text">12</div>
+	</div>
+	<div class="team-indicator left">
+		<div class="name">Venusaur</div>
+		<div class="cp">CP 1500</div>
+		<div class="balls">
+			<div class="ball"></div>
+			<div class="ball"></div>
+			<div class="ball"></div>
+		</div>
+		<div class="shields left">2</div>
+		<div class="cmp">CMP</div>
+		<div class="types">
+			<div class="type">G</div>
+			<div class="type">P</div>
+		</div>
+		<div class="iv-info-box">
+            <div class="iv-info-box-stat">15-15-15</div>
+            <div class="iv-info-box-rank">(#3)</div>
+		</div>
+	</div>
+
+	<div class="team-indicator right">
+		<div class="cp">CP 1500</div>
+		<div class="name">Venusaur</div>
+		<div class="shields right">2</div>
+		<div class="balls">
+			<div class="ball"></div>
+			<div class="ball"></div>
+			<div class="ball"></div>
+		</div>
+		<div class="featured-team-description">
+			<img>
+		</div>
+		<div class="cmp">CMP</div>
+		<div class="types">
+			<div class="type">G</div>
+			<div class="type">P</div>
+		</div>
+		<div class="iv-info-box">
+            <div class="iv-info-box-stat">15-15-15</div>
+            <div class="iv-info-box-rank">(#3)</div>
+		</div>
+	</div>
+</div>
