21bb3befaf7773a3160cc0c6b53f5efce3c56c0f
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2024-02-03 01:09:19 +0100

    page structure for the new simulation tab

---
+++ src/simulate/modules/controls.php
@@ -0,0 +1,124 @@
+<div class="button-stack">
+    <div class="pause-btn"></div>
+    <div class="restart-btn"></div>
+    <div class="quit-btn">&cross;</div>
+</div>
+
+<div class="player-controls">
+	<div class="move-labels">
+		<div class="label">Thunderbolt</div>
+		<div class="label">Magnet Bomb</div>
+	</div>
+
+	<div class="move-bars">
+		<div class="move-bar">
+			<div class="label">T</div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar-back electric"></div>
+		</div>
+		<div class="move-bar">
+			<div class="label">MB</div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar-back steel"></div>
+		</div>
+	</div>
+</div>
+<div class="opponent-controls">
+	<div class="move-labels">
+		<div class="label">Thunderbolt</div>
+		<div class="label">Magnet Bomb</div>
+	</div>
+
+	<div class="move-bars">
+		<div class="move-bar">
+			<div class="label">T</div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar-back electric"></div>
+		</div>
+		<div class="move-bar">
+			<div class="label">MB</div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar-back steel"></div>
+		</div>
+	</div>
+</div>
+
+<div class="player-switch-sidebar active">
+	<div class="container">
+		<div class="pokemon-container">
+			<div class="pokemon">
+				<div class="sprite-container">
+					<div class="cp">cp 1500</div>
+					<div class="main-sprite"></div>
+					<div class="secondary-sprite"></div>
+					<div class="health"></div>
+				</div>
+				<div class="name">Pokemon 1</div>
+				<div class="switch-timer">60</div>
+			</div>
+			<div class="pokemon">
+				<div class="sprite-container">
+					<div class="cp">cp 1500</div>
+					<div class="main-sprite"></div>
+					<div class="secondary-sprite"></div>
+					<div class="health"></div>
+				</div>
+				<div class="name">Pokemon 2</div>
+				<div class="switch-timer">60</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="opponent-switch-sidebar active">
+	<div class="container">
+		<div class="pokemon-container">
+			<div class="pokemon">
+				<div class="sprite-container">
+					<div class="cp">cp 1500</div>
+					<div class="main-sprite"></div>
+					<div class="secondary-sprite"></div>
+					<div class="health"></div>
+				</div>
+				<div class="name">Pokemon 1</div>
+				<div class="switch-timer">60</div>
+			</div>
+			<div class="pokemon">
+				<div class="sprite-container">
+					<div class="cp">cp 1500</div>
+					<div class="main-sprite"></div>
+					<div class="secondary-sprite"></div>
+					<div class="health"></div>
+				</div>
+				<div class="name">Pokemon 2</div>
+				<div class="switch-timer">60</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="restart-confirm hide">
+	<p>Restart the match?</p>
+
+	<div class="center flex">
+		<div class="button yes">Yes</div>
+		<div class="button no">No</div>
+	</div>
+</div>
+
+<div class="quit-confirm hide">
+	<p>Quit the match? In Tournament Mode, you'll go back to the team selection screen.</p>
+
+	<div class="center flex">
+		<div class="button yes">Yes</div>
+		<div class="button no">No</div>
+	</div>
+</div>
